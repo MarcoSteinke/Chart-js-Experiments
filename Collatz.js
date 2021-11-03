8955928e2dae669c60f6243f4c68e42b8cc4f5b0
maste150hhu <mastremolten2@gmail.com>
2021-11-03 10:19:46 +0100

    refactor to be OOP

--- Collatz.js
+++ Collatz.js
@@ -7,11 +7,11 @@ class Collatz {
     recur(coll = [], n) {
         coll.push(n);
         if(n == 1) return coll;
-        return (n % 2 == 0) ? recur(coll, n/2) : recur(coll, 3*n+1);
+        return (n % 2 == 0) ? this.recur(coll, n/2) : this.recur(coll, 3*n+1);
     }
       
     collatz(n) {
-        return recur([], n);
+        return this.recur([], n);
     }
 
     getResult() {
